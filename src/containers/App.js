f842b0eadb7eea7a50961e0cd969d165e1a152a2
gisellerx <7835369@gmail.com>
2022-08-10 19:19:19 -0400

    My version of robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -15,8 +16,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -25,21 +26,23 @@ class App extends Component {
 
   render() {
     const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
+    <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f2'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
             <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
 }
 
-export default App;
+export default App; 
